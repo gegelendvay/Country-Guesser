44f2b1e34641f7fd7150511b85e33a4d774b68ab
Gergely Lendvay <gege.lendvay2003@gmail.com>
2023-05-24 11:07:47 +0200

    Restart game functionality

--- client.py
+++ client.py
@@ -8,66 +8,76 @@ functions.register('Joe', '1234')
 
 def handleGame():
     global highScore
-    print('1. Play Again\n2. Leaderboard\n3. Login/Signup')
+    print('1. Play Again\n2. Leaderboard\n3. Login\n4.Register\n5. Quit')
     userInput = input()
     if not userInput.isdigit():
-        return
+        return False
 
     userInput = int(userInput)
     if userInput == 1:
         return True
     elif userInput == 2:
         print(functions.leaderboard())
-        return
+        return False
     elif userInput == 3:
         username = input('Enter your username: ')
         functions.login(username, '1234')
         functions.updateScore(username, highScore)
-        return
-    else:
-        print('Invalid input')
-        return
+        return False
+    elif userInput == 4:
+        username = input('Enter your username: ')
+        password = input('Enter your password: ')
+        functions.register(username, password)
+        return False
+    elif userInput == 5 or userInput > 5:
+        return False
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    port = 9999
-    s.connect(('localhost', port))
-    print(f'Connected to port {port}')
-    welcome = s.recv(1024).decode('utf-8')
-    print(welcome)
-    while True:
-        #Question
-        question = s.recv(4096).decode('utf-8')
-        print(question)
-        try:
-            options = json.loads(s.recv(1024).decode('utf-8'))
-            for i in options:
-                print(f'- {i}')
-        except json.decoder.JSONDecodeError:
-            s.close()
-            break
+    #return False
 
-        #Answer
-        answer = input('Enter your answer: ')
-        s.sendall(answer.encode('utf-8'))
+restart = True
 
-        #Result
-        response = s.recv(1024).decode('utf-8')
-        print(response)
+while restart:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        port = 9999
+        s.connect(('localhost', port))
+        print(f'Connected to port {port}')
+        welcome = s.recv(1024).decode('utf-8')
+        print(welcome)
+        while True:
+            #Question
+            question = s.recv(4096).decode('utf-8')
+            print(question)
+            try:
+                options = json.loads(s.recv(1024).decode('utf-8'))
+                for i in options:
+                    print(f'- {i}')
+            except json.decoder.JSONDecodeError:
+                s.close()
+                break
 
-        if response == 'Correct answer!':
-            continue
-
-        hint = input('Yes/No: ')
-        s.sendall(hint.encode('utf-8'))
-        if hint.lower() == 'yes' or hint.lower() == 'y':
-            continent = s.recv(1024).decode('utf-8')
-            print(continent)
+            #Answer
             answer = input('Enter your answer: ')
             s.sendall(answer.encode('utf-8'))
+
+            #Result
             response = s.recv(1024).decode('utf-8')
             print(response)
-        else:
-            print(s.recv(1024).decode('utf-8'))
-            handleGame()
-            break
+
+            if response == 'Correct answer!':
+                continue
+
+            hint = input('Yes/No: ')
+            s.sendall(hint.encode('utf-8'))
+            if hint.lower() == 'yes' or hint.lower() == 'y':
+                continent = s.recv(1024).decode('utf-8')
+                print(continent)
+                answer = input('Enter your answer: ')
+                s.sendall(answer.encode('utf-8'))
+                response = s.recv(1024).decode('utf-8')
+                print(response)
+            else:
+                print(s.recv(1024).decode('utf-8'))
+                restart = False
+                break
+    restart = handleGame()
 s.close()
