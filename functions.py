1f9a71ccdb04fe2401c8407af73c7e22cdb479e6
Gergely Lendvay <gege.lendvay2003@gmail.com>
2023-06-06 02:42:52 +0200

    Function documentation

--- functions.py
+++ functions.py
@@ -1,7 +1,8 @@
 import json
 import random
 import sqlite3
-    
+
+#Initialize database
 def setupDB():
     connection = sqlite3.connect('quiz.db')
     cursor = connection.cursor()
@@ -9,41 +10,46 @@ def setupDB():
     connection.commit()
     connection.close()
 
+#Get a random country
 def getCountry():
     with open("quiz.json") as f:
         data = json.load(f)
     country = random.choice(list(data.keys()))
     return country
 
+#Get the capital of the given country
 def getCapital(country: str):
     with open("quiz.json") as f:
         data = json.load(f)
     return data[country]['capital']
 
+#Get the continent of the given country
 def getContinent(country: str):
     with open("quiz.json") as f:
         data = json.load(f)
     return data[country]['continent']
 
+#Construct a question with the given country
 def getQuestion(country: str):
-    with open("quiz.json") as f:
-        data = json.load(f)
-    
     correctAnswer = country
     options = [correctAnswer]
+    #Get 3 random additional countries, and add them to the options list
     while len(options) <= 3:
         newCountry = getCountry()
         if newCountry not in options and newCountry != correctAnswer:
             options.append(newCountry)
+    #Shuffle the options list
     random.shuffle(options)
     return options
 
+#Check if the given answer is correct
 def checkAnswer(answer: str, correctAnswer: str):
     if answer.lower() == correctAnswer.lower():
         return True
     else:
         return False
 
+#Return the top 5 scoring players' usernames and scores
 def leaderboard():
     connection = sqlite3.connect('quiz.db')
     cursor = connection.cursor()
@@ -51,17 +57,20 @@ def leaderboard():
     result = cursor.fetchall()
     connection.close()
     return result
-    
+
+#Combined login and register function
 def login(username: str, password: str):
     connection = sqlite3.connect('quiz.db')
     cursor = connection.cursor()
     cursor.execute('SELECT username, password FROM quiz WHERE username=?', (username,))
     result = cursor.fetchone()
+    #If the username doesn't exist, create a new user
     if result is None:
         cursor.execute('INSERT INTO quiz VALUES (?, ?, ?)', (username, password, 0))
         connection.commit()
         connection.close()
         return True
+    #If it does exist, check if the password is correct or not
     else:
         if result[1] == password:
             connection.close()
@@ -70,6 +79,7 @@ def login(username: str, password: str):
             connection.close()
             return False
 
+#Update the user's high score
 def updateScore(username: str, score: int):
     connection = sqlite3.connect('quiz.db')
     cursor = connection.cursor()
@@ -77,6 +87,7 @@ def updateScore(username: str, score: int):
     result = cursor.fetchone()
     if result is not None:
         currentScore = result[0]
+        #Choose the bigger score: the currently stored high score or the score from the game
         newScore = max(score, currentScore)
         cursor.execute('UPDATE quiz SET highScore=? WHERE username=?', (newScore, username))
         connection.commit()
@@ -86,6 +97,7 @@ def updateScore(username: str, score: int):
         connection.close()
         return False
 
+#Reset the user's high score to 0, if the user exists
 def resetScore(username: str):
     connection = sqlite3.connect('quiz.db')
     cursor = connection.cursor()
