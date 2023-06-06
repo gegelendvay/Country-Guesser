d57faa34acc96c11b335bac1aaabc2933dd184c2
Gergely Lendvay <gege.lendvay2003@gmail.com>
2023-06-06 02:35:04 +0200

    Resturctured client and server files, added docs

--- client.py
+++ client.py
@@ -5,95 +5,126 @@ import os
 import socket
 import time
 
-def handleGame(score: int):
-    print('[1]Play Again\n[2]Leaderboard\n[3]Save Score\n[4]Reset Score\n[5]Quit')
-    userInput = input()
-    if not userInput.isdigit():
-        return False
+def handleGame(s):
+    score = 0
+    while True:
+        #Question
+        print(s.recv(4096).decode('utf-8'))
+        try:
+            #Decode the options and print them
+            #Since I had issues encoding lists, I had to use json
+            options = json.loads(s.recv(1024).decode('utf-8'))
+            for i in options:
+                print(f'- {i}')
+        except json.decoder.JSONDecodeError:
+            s.close()
+            break
 
-    userInput = int(userInput)
-    clearConsole()
-    if userInput == 1:
-        return True
-    elif userInput == 2:
-        leaderboard = functions.leaderboard()
-        print('Top 5 players:')
-        for i, j in enumerate(leaderboard):
-            print(f'{i+1}. {j[0]}: {j[1]}')
-        return False
-    elif userInput == 3:
-        username = input('Username: ')
-        password = getpass.getpass()
-        if functions.login(username, password):
-            time.sleep(1)
-            if functions.updateScore(username, int(score)):
-                print(f'Score saved for {username}!')
+        #Answer
+        answer = input('Enter your answer: ')
+        s.sendall(answer.encode('utf-8'))
+
+        #Result
+        response = s.recv(1024).decode('utf-8')
+        print(response)
+
+        #If the answer is correct, continue to the next question
+        if response == 'Correct answer!':
+            continue
+
+        hint = input('Yes/No: ')
+        s.sendall(hint.encode('utf-8'))
+        if hint.lower() == 'yes' or hint.lower() == 'y':
+            #Continent
+            print(s.recv(1024).decode('utf-8'))
+            answer = input('Enter your answer: ')
+            s.sendall(answer.encode('utf-8'))
+            #Result
+            print(s.recv(1024).decode('utf-8'))
+        else:
+            #If the user doesn't want a hint, get the score and end the game cycle
+            response = s.recv(1024).decode('utf-8')
+            print(response)
+            score = int(response.split(': ', 1)[1])
+            break
+
+    while True:
+        #If the game is over, provide the user with different options to choose from
+        print('[1]Play Again\n[2]Leaderboard\n[3]Save Score\n[4]Reset Score\n[5]Quit')
+        userInput = input()
+        #If the input is not a number, close the connection and end the game
+        if not userInput.isdigit():
+            return False
+
+        userInput = int(userInput)
+        clearConsole()
+        #Play again
+        if userInput == 1:
+            return True
+        #Leaderboard
+        elif userInput == 2:
+            leaderboard = functions.leaderboard()
+            #Check if leaderboard is empty or not
+            if len(leaderboard) != 0:
+                print('Top 5 players:')
+                for i, j in enumerate(leaderboard):
+                    print(f'{i+1}. {j[0]}: {j[1]} points')
             else:
-                print(f'Error saving score.')
-        else:
-            print('Invalid credentials.')
-        return False
-    elif userInput == 4:
-        username = input('Username: ')
-        password = getpass.getpass()
-        if functions.login(username, password):
-            if functions.resetScore(username):
-                print(f'Score reset for {username}!')
-        else:
-            print('Invalid credentials.')
-        return False
-    elif userInput > 4:
-        return False
-    
+                print('No scores saved yet.')
+            return False
+        #Save score
+        elif userInput == 3:
+            #Get username and password
+            username = input('Username: ')
+            #This method hides the password when inputting it
+            password = getpass.getpass()
+            #If the login or signup is successful, save the score
+            if functions.login(username, password):
+                #Save score
+                if functions.updateScore(username, int(score)):
+                    print(f'Score saved for {username}!')
+                else:
+                    print(f'Error saving score.')
+            else:
+                print('Invalid credentials.')
+            return False
+        #Reset score
+        elif userInput == 4:
+            #Get username and password
+            username = input('Username: ')
+            password = getpass.getpass()
+            #If the login was successful, reset the score to 0 without deleting the user
+            if functions.login(username, password):
+                if functions.resetScore(username):
+                    print(f'Score reset for {username}!')
+            else:
+                print('Invalid credentials.')
+            return False
+        #Quit
+        elif userInput > 4:
+            return False
+
+#Clear command depending on the operating system the program is running on
 def clearConsole():
     os.system('cls' if os.name=='nt' else 'clear')
 
-restart = True
-score = 0
-clearConsole()
-
-while restart:
+#Main function handling the game connection
+def playGame():
+    #Establish socket connection
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         port = 9999
         s.connect(('localhost', port))
-        #Welcome
+        #Welcome message
         print(s.recv(1024).decode('utf-8'))
-        while True:
-            #Question
-            print(s.recv(4096).decode('utf-8'))
-            try:
-                options = json.loads(s.recv(1024).decode('utf-8'))
-                for i in options:
-                    print(f'- {i}')
-            except json.decoder.JSONDecodeError:
-                s.close()
-                break
+        state = handleGame(s)
+    return state
 
-            #Answer
-            answer = input('Enter your answer: ')
-            s.sendall(answer.encode('utf-8'))
+restart = True
+clearConsole()
 
-            #Result
-            response = s.recv(1024).decode('utf-8')
-            print(response)
-
-            if response == 'Correct answer!':
-                continue
-
-            hint = input('Yes/No: ')
-            s.sendall(hint.encode('utf-8'))
-            if hint.lower() == 'yes' or hint.lower() == 'y':
-                #Continent
-                print(s.recv(1024).decode('utf-8'))
-                answer = input('Enter your answer: ')
-                s.sendall(answer.encode('utf-8'))
-                #Result
-                print(s.recv(1024).decode('utf-8'))
-            else:
-                response = s.recv(1024).decode('utf-8')
-                print(response)
-                score = int(response.split(': ', 1)[1])
-                restart = False
-                break
-    restart = handleGame(score)
-s.close()
+#Main loop handling the game restart
+while restart:
+    restart = playGame()
+    #If the user wants to play again, clear the console
+    if restart:
+        clearConsole()
--- server.py
+++ server.py
@@ -3,54 +3,84 @@ import json
 import socket
 import threading
 
+threadingLock = threading.Lock()
+
 def handleClient(client, address):
+    #Initialize the game
     points = 0
-    client.sendall('Welcome to the country quiz game'.encode('utf-8'))
+    client.sendall('Welcome to the country quiz game!'.encode('utf-8'))
+    
     while True:
-        country = functions.getCountry()
-        capital = functions.getCapital(country)
-        question = f'Which countries capital city is {capital}?'
-        client.sendall(question.encode('utf-8'))
-        options = functions.getQuestion(country)
-        client.sendall(json.dumps(options).encode('utf-8'))
+        #Use the lock to prevent conflicts between threads
+        with threadingLock:
+            #Get a random country and its capital
+            country = functions.getCountry()
+            capital = functions.getCapital(country)
+            #Send the question and the options to the client
+            question = f'Which country\'s capital city is {capital}?'
+            client.sendall(question.encode('utf-8'))
+
+            options = functions.getQuestion(country)
+            client.sendall(json.dumps(options).encode('utf-8'))
+
         try:
             answer = client.recv(4096).decode('utf-8')
         except UnicodeDecodeError:
             break
+
+        #Compare the given answer with the correct answer
         if functions.checkAnswer(answer, country):
             client.sendall('Correct answer!'.encode('utf-8'))
+            #Add 2 points to the score
             points += 2
-            continue
         else:
+        #If the answer is incorrect, ask the user if they want a hint or not
             client.sendall('Incorrect answer. Do you want a hint?'.encode('utf-8'))
             hint = client.recv(1024).decode('utf-8')
             if hint.lower() == 'yes' or hint.lower() == 'y':
+                #If the user wants a hint, send the continent to the client
                 continent = functions.getContinent(country)
                 client.sendall(f'The country is located in {continent}.'.encode('utf-8'))
                 try:
                     hintAnswer = client.recv(1024).decode('utf-8')
                 except UnicodeDecodeError:
                     break
+                #Compare the given answer with the correct answer
                 if functions.checkAnswer(hintAnswer, country):
                     client.sendall('Correct answer!'.encode('utf-8'))
+                    #Add 1 point to the score if the answer is correct with the use of a hint
                     points += 1
-                    continue
                 else:
+                    #If the answer is incorrect, end the game
                     client.sendall(f'Incorrect answer. The correct answer was {country}! Your score: {points}'.encode('utf-8'))
                     client.close()
                     break
             else:
+                #If the user doesn't want a hint, send the score to the client and end the game
                 client.sendall(f'Thanks for playing! Your score: {points}'.encode('utf-8'))
                 client.close()
                 break
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    port = 9999
-    s.bind(('localhost', port))
-    s.listen()
-    print(f'Listening on port {port}')
-    functions.setupDB()
-    while True:
-        client, address = s.accept()
-        thread = threading.Thread(target=handleClient, args=(client, address))
-        thread.start()
+def startServer():
+    #List of client threads
+    threads = []
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        port = 9999
+        s.bind(('localhost', port))
+        s.listen()
+        print(f'Listening on port {port}')
+        #Initialize the database
+        functions.setupDB()
+        while True:
+            client, address = s.accept()
+            #Start a new thread for each client connection
+            thread = threading.Thread(target=handleClient, args=(client, address))
+            thread.start()
+            threads.append(thread)
+
+            # Wait for all client threads to finish before closing the server
+            for thread in threads:
+                thread.join()
+
+startServer()
